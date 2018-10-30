519e024510f2c570813f57dfd3f88b5ccad8286d
liuyingjie <845919725@qq.com>
2018-10-30 11:18:58 +0800

    commit v1.0.3 and update demo

--- demo/InterviewDemo/app/src/main/java/com/zijingdemo/MyVideoActivity.java
+++ demo/InterviewDemo/app/src/main/java/com/zijingdemo/MyVideoActivity.java
@@ -1,74 +1,106 @@
 package com.zijingdemo;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 
+import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.zjrtc.ZjCall;
 import com.zjrtc.ZjCallListenerBase;
-import com.zjrtc.ZjVideoActivity;
+import com.zjrtc.ZjRTCViewManager;
 import com.zjrtc.ZjVideoManager;
 
-public class MyVideoActivity extends ZjVideoActivity {
+public class MyVideoActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
 
-    private static final String TAG = "MyVideoActivity";
+    private final String TAG = "MyVideoActivity";
+
+    private ReactRootView mReactRootView;
+
+    private ZjCall call;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = LayoutInflater.from(this).inflate(R.layout.video_layout, null);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
-        addContentView(view, params);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.video_layout);
 
-        //强制打开扬声器
-        ZjVideoManager.getInstance().openSpeaker(this,true);
+        call = (ZjCall) getIntent().getSerializableExtra("call");
+        //设置呼叫参数
+        ZjVideoManager.getInstance().setCall(call);
+        //初始化ZjRTCViewManager
+        ZjRTCViewManager.init(getApplication());
+        //获取rootView
+        mReactRootView = ZjRTCViewManager.getRootView(this);
+        //打开reactApplication
+        ZjRTCViewManager.startReactApplication();
+
+        //显示rootView
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(dm.widthPixels,dm.widthPixels);
+        params.gravity = Gravity.TOP;
+        addContentView(mReactRootView,params);
 
         setListener();
     }
 
-    private void setListener() {
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ZjRTCViewManager.onResume(this,this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        ZjRTCViewManager.onDestory(this);
+        ZjVideoManager.getInstance().release();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //传递返回按钮点击事件（退会操作），不设置可以自己处理返回按钮事件，但要记得做退会操作。
+        ZjRTCViewManager.onBackPressed();
+    }
+
+    private void setListener(){
         ZjVideoManager.getInstance().addZjCallListener(new ZjCallListenerBase(){
-
-            @Override
-            public void callState(String state, String info) {
-                Log.v(TAG,state + ":" + info);
-            }
-
             @Override
             public void videoState(ReadableArray params) {
-                Log.v(TAG, "videoState:"+params);
-            }
-
-            @Override
-            public void onMuteChanged(boolean muted) {
-                Log.v(TAG,"onMuteChange:"+muted);
-            }
-
-            @Override
-            public void onCameraState(boolean isPause) {
-                Log.v(TAG,"onCameraState:"+isPause);
-            }
-
-            @Override
-            public void onMicrophoneState(boolean isPause) {
-                Log.v(TAG,"onMicrophoneState"+isPause);
+                Log.i(TAG,params.toString());
+                super.videoState(params);
             }
 
             @Override
             public void onChatMessage(String uuid, String message) {
-                Log.v(TAG,"onChatMessage"+uuid+message);
+                Log.i(TAG,message);
+                super.onChatMessage(uuid, message);
             }
 
             @Override
             public void onParticipantsUpdate(ReadableArray participants) {
-                Log.v(TAG,"participants"+participants);
+                Log.i(TAG,"participants:" + participants);
+                super.onParticipantsUpdate(participants);
             }
         });
     }
@@ -93,9 +125,13 @@ public class MyVideoActivity extends ZjVideoActivity {
         ZjVideoManager.getInstance().sendMessage("哈哈哈");
     }
 
+    public void reconnect(View v){
+        ZjVideoManager.getInstance().reconnect();
+    }
+
     public void callOut(View v){
-        final EditText editText = new EditText(MyVideoActivity.this);
-        final AlertDialog.Builder editDialog = new AlertDialog.Builder(MyVideoActivity.this);
+        final EditText editText = new EditText(this);
+        final AlertDialog.Builder editDialog = new AlertDialog.Builder(this);
         editDialog.setTitle("请输入外呼地址")
                 .setView(editText).setNegativeButton("取消",null)
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
