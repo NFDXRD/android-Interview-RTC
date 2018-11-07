267693088de5be0b03a2ee103501b2712dbaa2e6
liuyingjie <845919725@qq.com>
2018-11-07 17:25:09 +0800

    commit version 1.0.7

--- demo/InterviewDemo/app/src/main/java/com/zijingdemo/InterviewActivity.java
+++ demo/InterviewDemo/app/src/main/java/com/zijingdemo/InterviewActivity.java
@@ -33,7 +33,8 @@ public class InterviewActivity extends AppCompatActivity {
                 //构建呼叫参数类，设置显示名称、呼叫地址、呼叫密码；
                 ZjCall call = new ZjCall();
                 call.setDisplayName("面试官");
-                call.setAddress("2216");
+                //会议室地址和密码不设置，使用默认设置
+                call.setAddress("9343");
                 call.setPwd("123456");//会议室主持人密码
                 call.setCheckDup(MD5Util.MD5(Build.MODEL+"面试官"));
                 call.setInterviewer(true);
@@ -50,8 +51,9 @@ public class InterviewActivity extends AppCompatActivity {
                 //构建呼叫参数类，设置显示名称、呼叫地址、呼叫密码；
                 ZjCall call = new ZjCall();
                 call.setDisplayName("应聘者");
-                call.setAddress("2216");
-                call.setPwd("");//会议室访客密码
+                //会议室地址和密码不设置，使用默认设置
+                call.setAddress("9343");
+                call.setPwd("123456");//会议室访客密码
                 call.setCheckDup(MD5Util.MD5(Build.MODEL+"应聘者"));
                 call.setInterviewer(false);
 
--- demo/InterviewDemo/app/src/main/java/com/zijingdemo/MyVideoActivity.java
+++ demo/InterviewDemo/app/src/main/java/com/zijingdemo/MyVideoActivity.java
@@ -1,21 +1,14 @@
 package com.zijingdemo;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.ReadableArray;
